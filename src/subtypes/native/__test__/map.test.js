5b67ddcbbdbc66b66c3222d3e84c87b64f4160c6
Conrad Buck <conartist6@gmail.com>
2019-01-15 08:26:37 -0800

    remove native for now

--- src/subtypes/native/__test__/map.test.js
+++
@@ -1,7 +0,0 @@
-import SequinsMap from '../map';
-
-describe('SequinsMap', function() {
-  it('is a subtype of native Map', function() {
-    expect(SequinsMap.prototype).toBeInstanceOf(Map);
-  });
-});
--- src/subtypes/native/__test__/set.test.js
+++
@@ -1,7 +0,0 @@
-import SequinsSet from '../set';
-
-describe('SequinsSet', function() {
-  it('is a subtype of native Set', function() {
-    expect(SequinsSet.prototype).toBeInstanceOf(Set);
-  });
-});
